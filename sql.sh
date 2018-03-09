c9dfd10ad0e1c343cc29b1257da069091e2908cd
wentingt <wentingt@umich.edu>
2018-03-08 21:07:50 -0700

    get json format of explain command for all tpcds queries with(out) cbo

--- sql.sh
+++ sql.sh
@@ -1,24 +1,19 @@
-DIR=$1
-database=$2
-explain=${3:-true}
+database=$1
+cbo=${2:-true}
+TESTHOME=/users/wentingt/hive-testbench
+INPUT=$TESTHOME/sample-queries-tpcds
+OUTPUT=$TESTHOME/trees/${database}-${cbo}
+rm -r $OUTPUT
+mkdir -p $OUTPUT
 
-log_database=log_$database
-err_database=err_$database
-
-rm $log_database $err_database
-
-for filename in $DIR/*.sql; do
+cd $INPUT
+for filename in *.sql; do
 	echo $filename
-        echo "@@@$filename" >>$log_database
-        echo "@@@$filename" >>$err_database
 
-	if $explain; then
-		filename_=${filename}_
-                cp $filename $filename_
-		awk '/^$/ {nlstack=nlstack "\n";next;} {printf "%s",nlstack; nlstack=""; print;}' $filename | tac | sed 's/\;/\; explain FORMATTED/g' |  sed '1 s/explain FORMATTED//' | tac > $filename_
-    		hive --database $database -e "set hive.execution.engine=tez; explain FORMATTED $(<$filename_)" 1>>$log_database 2>>$err_database
-                rm $filename_             
-	else 
-        	hive --database $database -f $filename 1>>$log_database 2>>$err_database
-	fi
+	filename_=${filename}_
+	cp $filename $filename_
+	awk '/^$/ {nlstack=nlstack "\n";next;} {printf "%s",nlstack; nlstack=""; print;}' $filename | tac | sed 's/\;/\; explain FORMATTED/g' |  sed '1 s/explain FORMATTED//' | tac > $filename_
+	hive --database $database -e "set hive.cbo.enable=$cbo; explain FORMATTED $(<$filename_)" 1>$OUTPUT/${filename}.json 
+	rm $filename_             
+
 done

a75e54565df6ad0f810c2b40e2d32d95e74e54a6
wentingt <wentingt@umich.edu>
2018-03-12 15:43:16 -0600

    intermediate plans in trees/.../*.alldags

--- sql.sh
+++ sql.sh
@@ -5,17 +5,19 @@ OUTPUT=$TESTHOME/trees/${database}
 rm -r $OUTPUT
 mkdir -p $OUTPUT
 HIVELOG=/tmp/wentingt/hive.log
+QUERYLOG=/tmp/crossquery
 
 cd $INPUT
 for filename in *.sql; do
 	echo $filename
-	rm $HIVELOG
 
 	filename_=${filename}_
 	cp $filename $filename_
 	awk '/^$/ {nlstack=nlstack "\n";next;} {printf "%s",nlstack; nlstack=""; print;}' $filename | tac | sed 's/\;/\; explain FORMATTED/g' |  sed '1 s/explain FORMATTED//' | tac > $filename_
-	hive --database $database -e "set hive.crossquery.combination=-1; explain FORMATTED $(<$filename_)" 1>$OUTPUT/${filename}.json >>$OUTPUT/log
+	hive --database $database -e "set hive.crossquery.verbose=true; set hive.crossquery.combination=-1; explain FORMATTED $(<$filename_)" 1>$OUTPUT/${filename}.json 2>>$OUTPUT/log
 	rm $filename_             
 
-	cp $HIVELOG $OUTPUT/${filename}.log
+	mv $HIVELOG $OUTPUT/${filename}.log
+	mv $QUERYLOG $OUTPUT/${filename}
+
 done

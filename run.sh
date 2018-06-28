abdcba472f4ef90e982ba44b0a69e61d106c0167
jimmyyou <wentingt@umich.edu>
2018-06-28 10:01:56 -0400

    tpch

--- run.sh
+++ run.sh
@@ -7,14 +7,14 @@ SPARK_SQL_SHUFFLE_PARTITIONS=40
 SPARK_DRIVER_MEMORY=4g
 SPARK_EXECUTOR_MEMORY=4g
 
-for filename in sample-queries-tpcds/*.sql; do
+for filename in sample-queries-tpch/*.sql; do
 	echo $filename
 	$SPARK_HOME/bin/spark-sql \
 		--conf spark.driver.memory=$SPARK_DRIVER_MEMORY \
 		--conf spark.executor.memory=$SPARK_EXECUTOR_MEMORY \
 		--conf spark.cores.max=$SPARK_CORES_MAX \
 		--conf spark.sql.shuffle.partitions=$SPARK_SQL_SHUFFLE_PARTITIONS \
-		--database tpcds_text_10 \
+		--database tpch_text_10 \
 		-f $filename \
 		1>${filename}.${version}.${seq}.log 2>${filename}.${version}.${seq}.err
 	#break 

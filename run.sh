1e2e14ca6c94e209f9d717e4240155b69d3b0d42
wentingt <wentingt@umich.edu>
2018-06-28 00:22:02 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 version=${1:-spark}
 
+SPARK_CORES_MAX=20
+SPARK_SQL_SHUFFLE_PARTITIONS=4
+
 for filename in sample-queries-tpcds/*.sql; do
 	echo $filename
 	$SPARK_HOME/bin/spark-sql \
-		--conf spark.cores.max=20 \
-		--conf spark.sql.shuffle.partitions=4 \
+		--conf spark.cores.max=SPARK_CORES_MAX \
+		--conf spark.sql.shuffle.partitions=SPARK_SQL_SHUFFLE_PARTITIONS \
 		--database tpcds_text_10 \
 		-f $filename \
 		1>${filename}.${version}.log 2>${filename}.${version}.err
-	break
 done 

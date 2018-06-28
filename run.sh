fa804316d9be828ba0b1da03db3244783a211555
wentingt <wentingt@umich.edu>
2018-06-27 23:55:29 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-#export SPARK_HOME=/users/jimmyyou/spark-terra
-export SPARK_HOME=/users/jimmyyou/spark-2.3.1-bin-hadoop2.7
+version=${1:-spark}
 
 for filename in sample-queries-tpcds/*.sql; do
 	echo $filename
@@ -8,13 +7,6 @@ for filename in sample-queries-tpcds/*.sql; do
 		--conf spark.sql.shuffle.partitions=4 \
 		--database tpcds_text_10 \
 		-f $filename \
-		1>${filename}.log 2>${filename}.err
+		1>${filename}.${version}.log 2>${filename}.${version}.err
 	break
 done 
-
-exit 1
-for filename in sample-queries-tpch/*.sql; do
-	echo $filename
-	$SPARK_HOME/bin/spark-sql --database tpch_text_2 -f $filename 1>${filename}.log 2>${filename}.err
-done 
-
